4925e8cd3566e7d5d1f9bdb219b8e76785b6e5e5
Hafizur Rahman <99hafizurrahman@gmail.com>
2023-09-07 10:02:38 +0530

    team page update

--- src/Components/TeamMember.js
+++ src/Components/TeamMember.js
@@ -3,23 +3,23 @@ import { AiFillTwitterCircle } from "react-icons/ai";
 
 const TeamMember = (props) => {
   return (
-    <div className="flex flex-col items-center justify-start w-[300px] lg:w-[300px] border">
+    <div className="flex flex-col items-center justify-center w-[300px] lg:w-[300px] bg-white shadow-sm">
       <img
         className="object-cover w-[100%]   "
         src={props.member.photo}
         alt=""
       />
 
-      <div className="self-start m-4 justify-self-start font-Roboto">
+      <div className=" m-4  bg-white">
         <h1 className="mt-2 text-2xl font-semibold text-slate-900 capitalize  group-hover:text-white">
           {props.member.name}
         </h1>
 
-        <p className=" text-slate-800 capitalize text-[18px]  group-hover:text-gray-300">
+        {/* <p className=" text-slate-800 capitalize text-[18px]  group-hover:text-gray-300">
           {props.member.position}
-        </p>
+        </p> */}
 
-        <div className="flex mt-3 -mx-2 justify-self-end">
+        <div className="flex mt-3 justify-center">
           <a
             href={props.member.twitter}
             target="_blank"
