266a3db3af7b7fcce234adc4a32cb4ad6850488b
Hafizur Rahman <99hafizurrahman@gmail.com>
2023-09-08 16:02:47 +0530

    product update

--- src/Components/OurProudct.js
+++ src/Components/OurProudct.js
@@ -1,13 +1,16 @@
-import bg from "../assets/ourproduct.jpg";
+import bg from "../assets/ourproduct1.jpg";
 
 const OurProduct = () => {
   return (
     <div className="heroSection z-0 w-[100%]  h-[400px] overflow-hidden flex  ">
       <div className=" absolute z-10 w-[100%] h-[400px] flex flex-col justify-center items-center gap-2  text-white">
-        <h2 className="lg:text-[4rem] sm:text-[3rem] text-[2.5rem] font-bold  ">
+        <h2
+          className="lg:text-[4rem] sm:text-[3rem] text-[2.5rem] font-bold  "
+          data-aos="zoom-in"
+        >
           Our Products
         </h2>
-        <p className="lg:text-[1.8rem] font-normal mb-4 ">
+        <p className="lg:text-[1.8rem] font-normal mb-4 " data-aos="zoom-in">
           Solutions for a smarter, more connected future
         </p>
       </div>
