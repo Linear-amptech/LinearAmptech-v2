79f14cbc53a7fabefadda86accd01593cc7619ee
Hafizur Rahman <99hafizurrahman@gmail.com>
2023-09-09 23:07:18 +0530

    fix heihgt

--- src/Components/Slider.js
+++ src/Components/Slider.js
@@ -8,6 +8,36 @@ import "swiper/css/pagination";
 import "swiper/css/navigation";
 import { homeProduct } from "../data/HomeData";
 import "./Styles.css";
+const heading = ["", ,];
+const TEXTS = [""];
+
+const sliderData = [
+  {
+    title: "Linear Amptech",
+    content: "Innovating the Future of Cyber-Physical Systems",
+    img: "",
+  },
+  {
+    title: "Empowering Automation",
+    content: "Integrating Machine Learning into Real-World Systems",
+    img: "",
+  },
+  {
+    title: "Leading in RF Technology",
+    content: "Transforming Interactions with our Cyber-Physical System Designs",
+    img: "",
+  },
+  {
+    title: "Bridging the Digital and Physical",
+    content: "Innovating the Future of Cyber-Physical Systems",
+    img: "",
+  },
+  {
+    title: "Your Tech Partner for Tomorrow",
+    content: "Linear Amptech - Pioneering Breakthrough Solutions",
+    img: "",
+  },
+];
 
 const Slider = () => {
   return (
@@ -26,32 +56,21 @@ const Slider = () => {
         // dynamicBullets: true,
       }}
       modules={[Autoplay, Pagination, Navigation]}
-      className="mySwiper h-[500px]"
+      className="mySwiper h-[500px] flex items-center"
     >
-      {homeProduct.map(({ img, title, tag, content, link }) => {
+      {sliderData.map(({ img, title, content }) => {
         return (
           <SwiperSlide>
-            <div className="slide bg-[url('https://cdn.pixabay.com/photo/2023/07/19/22/28/amur-tiger-8138017_1280.jpg')] bg-cover bg-center flex flex-col lg:flex-row gap-20 justify-center items-center w-[100%] h-[100%] py-20 mx-auto bg-red-600">
-              <div className="lg:h-[400px] h-[100px] max-w-[450px] flex justify-center  items-center ">
-                <img src={img} className="min-w-[400px] "></img>
-              </div>
-              <div className="lg:right lg:max-w-[650px] flex flex-col gap-4">
-                <p className="tag text-[#0346B5] text-[1.1rem] font-medium ">
-                  {tag}
-                </p>
-                <h2 className="lg:text-[2.5rem] text-2xl lg:leading-normal font-bold">
-                  {title}
-                </h2>
-                <p className=" text-[0.9rem] leading-loose min-h-[200px] text-justify">
-                  {content}
-                </p>
-                <div>
-                  <a
-                    className="text-blue-900 underline font-medium cursor-pointer"
-                    href={link}
-                  >
-                    Read more
-                  </a>
+            <div className="bg-[black] w-full h-full">
+              <div className="slide container  text-white bg-cover bg-center flex  lg:flex-row gap-20 justify-start items-center w-[100%] h-[100%]  py-20 px-6 mx-auto">
+                <div className=" gap-4">
+                  <h2 className="lg:text-[3rem] text-2xl lg:leading-normal font-bold">
+                    {title}
+                  </h2>
+                  <p className=" text-[2rem] leading-loose min-h-[200px] text-justify">
+                    {content}
+                  </p>
+                  <div></div>
                 </div>
               </div>
             </div>
