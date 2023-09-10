b31af0b620b54f60efaa4c8a741afbb8a90f598c
Hafizur Rahman <99hafizurrahman@gmail.com>
2023-09-10 11:06:06 +0530

    bg added

--- src/Components/Slider.js
+++ src/Components/Slider.js
@@ -15,27 +15,27 @@ const sliderData = [
   {
     title: "Linear Amptech",
     content: "Innovating the Future of Cyber-Physical Systems",
-    img: "",
+    img: "https://static.vecteezy.com/system/resources/previews/005/301/851/non_2x/businessman-success-with-his-board-room-free-photo.jpg",
   },
   {
     title: "Empowering Automation",
     content: "Integrating Machine Learning into Real-World Systems",
-    img: "",
+    img: "https://static.vecteezy.com/system/resources/previews/017/075/645/non_2x/digital-transformation-technology-strategy-digitization-and-digitalization-of-business-processes-and-data-optimize-and-automate-operations-customer-service-management-internet-and-cloud-computing-free-photo.jpg",
   },
   {
     title: "Leading in RF Technology",
     content: "Transforming Interactions with our Cyber-Physical System Designs",
-    img: "",
+    img: "https://static.vecteezy.com/system/resources/previews/004/656/020/non_2x/transmitting-signals-through-communications-satellites-in-the-sky-and-the-capital-city-with-tall-buildings-free-photo.jpg",
   },
   {
     title: "Bridging the Digital and Physical",
     content: "Innovating the Future of Cyber-Physical Systems",
-    img: "",
+    img: "https://static.vecteezy.com/system/resources/previews/027/100/018/non_2x/the-idea-of-a-global-social-network-and-business-connection-is-a-concept-that-combines-the-future-free-photo.jpg",
   },
   {
     title: "Your Tech Partner for Tomorrow",
     content: "Linear Amptech - Pioneering Breakthrough Solutions",
-    img: "",
+    img: "https://static.vecteezy.com/system/resources/previews/007/252/575/non_2x/businessman-holding-global-internet-connection-technology-business-and-digital-marketing-free-photo.jpg",
   },
 ];
 
@@ -47,7 +47,7 @@ const Slider = () => {
       loop={true}
       speed={800}
       autoplay={{
-        delay: 5000,
+        delay: 3500,
         disableOnInteraction: false,
       }}
       pagination={{
@@ -56,15 +56,21 @@ const Slider = () => {
         // dynamicBullets: true,
       }}
       modules={[Autoplay, Pagination, Navigation]}
-      className="mySwiper h-[600px] flex items-center"
+      className="mySwiper h-[600px] flex items-center relative"
     >
-      {sliderData.map(({ img, title, content }) => {
+      {sliderData.map(({ title, content, img }) => {
         return (
           <SwiperSlide>
-            <div className="bg-[black] w-full h-full">
+            <div
+              className={` h-full w-full bg-cover bg-black`}
+              style={{
+                backgroundImage: `url("${img}")`,
+              }}
+            >
+              <div className="h-full w-full absolute bg-black/50"></div>
               <div className="slide container  text-white bg-cover bg-center flex  lg:flex-row gap-20 justify-start items-center w-[100%] h-[100%]  py-20 px-6 mx-auto">
-                <div className=" gap-4">
-                  <h2 className="lg:text-[3rem] text-4xl lg:leading-normal font-bold">
+                <div className=" gap-4 relative z-50">
+                  <h2 className="lg:text-[3rem] text-4xl lg:leading-normal ">
                     {title}
                   </h2>
                   <p className=" text-[2rem]  min-h-[200px] ">{content}</p>
