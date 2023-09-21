3cb1b237710d6ff88965409ffeb27bcb39b08311
Hafizur Rahman <99hafizurrahman@gmail.com>
2023-09-21 07:30:43 +0530

    partners added

--- src/Components/Partners.js
+++ src/Components/Partners.js
@@ -1,10 +1,15 @@
 import analog from "../assets/partners/analog.png";
 import ihub from "../assets/partners/ihub.png";
 import lekha from "../assets/partners/lekha.png";
+import skynet from "../assets/partners/skynet.webp";
+import apctech from "../assets/partners/apctech.webp";
 
 const partners = [
   { logo: lekha, name: "Lekha wireless Solution" },
   { logo: analog, name: "Analog Devices" },
+  { logo: skynet, name: "Skyenet Wireless" },
+  { logo: apctech, name: "APCTECH" },
+
   { logo: ihub, name: "TIH IIT Roorkee" },
 
   // Add more partners as needed
@@ -12,7 +17,7 @@ const partners = [
 
 const Partner = ({ logo, name }) => {
   return (
-    <div className="  flex  justify-center  items-center lg:w-[300px] lg:h-[260px] w-[260px] h-[220px]  nested-div py-[2.5rem]">
+    <div className="  flex  justify-center  items-center lg:w-[260px] lg:h-[260px] w-[260px] h-[220px]  nested-div py-[2.5rem]">
       <div className="w-auto h-[120px] flex justify-center items-center">
         <img
           src={logo}
