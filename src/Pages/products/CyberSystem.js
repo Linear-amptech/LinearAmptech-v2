2471ab992f3368e83f18b99bb0ab696bd5447321
Hafizur Rahman <99hafizurrahman@gmail.com>
2024-03-01 11:50:36 +0530

    update wait prduct page

--- src/Pages/products/CyberSystem.js
+++ src/Pages/products/CyberSystem.js
@@ -64,7 +64,7 @@ const CyberSystem = () => {
         <div className="lg:flex  justify-center gap-16 my-36 px-[10%]">
           <div className="lg:w-[400px] lg:h-[400px] md:w-[600px] md:h-[600px] h-[350px] w-[350px] bg-[#F2F2F2] flex justify-center rounded-2xl shadow-md">
             <img
-              src={require("../../assets/WaitSystem/1.png")}
+              src={require("../../assets/WaitSystem/wait1.png")}
               className="w-[100%]  "
             ></img>
           </div>
@@ -116,7 +116,7 @@ const CyberSystem = () => {
           </div>
           <div className="max-w-[550px] flex justify-center items-center">
             <img
-              src={require("../../assets/WaitSystem/3.png")}
+              src={require("../../assets/WaitSystem/wait4.png")}
               className="w-[400px]"
             ></img>
           </div>
