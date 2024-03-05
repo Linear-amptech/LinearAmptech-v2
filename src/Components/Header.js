3ccf4aadad0d601000be064ecabdf6c8ad24aa16
Hafizur Rahman <99hafizurrahman@gmail.com>
2024-03-05 14:57:12 +0530

    fix header

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -31,23 +31,23 @@ const Header = () => {
   const [prevScrollPos, setPrevScrollPos] = useState(0);
   const [visible, setVisible] = useState(true);
 
-  const handleScroll = () => {
-    const currentScrollPos = window.scrollY;
+  // const handleScroll = () => {
+  //   const currentScrollPos = window.scrollY;
 
-    if (currentScrollPos > prevScrollPos) {
-      setVisible(false);
-    } else {
-      setVisible(true);
-    }
+  //   if (currentScrollPos > prevScrollPos) {
+  //     setVisible(false);
+  //   } else {
+  //     setVisible(true);
+  //   }
 
-    setPrevScrollPos(currentScrollPos);
-  };
+  //   setPrevScrollPos(currentScrollPos);
+  // };
 
-  useEffect(() => {
-    window.addEventListener("scroll", handleScroll);
+  // useEffect(() => {
+  //   window.addEventListener("scroll", handleScroll);
 
-    return () => window.removeEventListener("scroll", handleScroll);
-  });
+  //   return () => window.removeEventListener("scroll", handleScroll);
+  // });
 
   const handleClick = () => {
     setIsOpen(!isOpen);
