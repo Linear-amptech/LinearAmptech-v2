23400dd2535bbf432cbd2b711202e4c93504bc6c
Hafizur Rahman <83756518+hafizurman@users.noreply.github.com>
2025-10-05 12:55:33 +0530

    fix(ui): remove team member

--- src/data/CoreTeamData.js
+++ src/data/CoreTeamData.js
@@ -1,12 +1,5 @@
-import devansh from "../assets/profile/devansh.jfif";
+
 
 export const coreTeam = [
-  {
-    photo: devansh,
-    name: "Devansh Joshi",
-    position: "Sprinklr",
-    linkedin: "https://www.linkedin.com/in/jdevansh/",
-    twitter: "#",
-    facebook: "#",
-  },
+
 ];
